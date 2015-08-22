a15216add43a0e8ccd147f45bda10b6f645e1905
cesarmind <maxrios6@gmail.com>
2015-08-22 00:37:06 -0400

    Cleanup 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,9 @@
 class PagesController < ApplicationController
+  
   def home
   end
-<<<<<<< HEAD
-  
-  def about
-=======
+
   def about 
->>>>>>> d7ecc4ad64ae2cd3a60923b91d1b7e40c2a7d9ff
   end
+  
 end
